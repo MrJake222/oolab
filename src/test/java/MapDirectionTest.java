1493d266a7358b7927872b374d45a85e9583c91d
MrJake222 <norbertzpilicy@gmail.com>
2021-10-28 13:46:15 +0200

    Static import of Assertions

--- src/test/java/MapDirectionTest.java
+++ src/test/java/MapDirectionTest.java
@@ -1,22 +1,23 @@
 import agh.ics.oop.MapDirection;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MapDirectionTest {
 
     @Test
     public void nextTest() {
-        Assertions.assertEquals(MapDirection.SOUTH, MapDirection.EAST.next());
-        Assertions.assertEquals(MapDirection.WEST, MapDirection.SOUTH.next());
-        Assertions.assertEquals(MapDirection.NORTH, MapDirection.WEST.next());
-        Assertions.assertEquals(MapDirection.EAST, MapDirection.NORTH.next());
+        assertEquals(MapDirection.SOUTH, MapDirection.EAST.next());
+        assertEquals(MapDirection.WEST, MapDirection.SOUTH.next());
+        assertEquals(MapDirection.NORTH, MapDirection.WEST.next());
+        assertEquals(MapDirection.EAST, MapDirection.NORTH.next());
     }
 
     @Test
     public void previousTest() {
-        Assertions.assertEquals(MapDirection.NORTH, MapDirection.EAST.previous());
-        Assertions.assertEquals(MapDirection.WEST, MapDirection.NORTH.previous());
-        Assertions.assertEquals(MapDirection.SOUTH, MapDirection.WEST.previous());
-        Assertions.assertEquals(MapDirection.EAST, MapDirection.SOUTH.previous());
+        assertEquals(MapDirection.NORTH, MapDirection.EAST.previous());
+        assertEquals(MapDirection.WEST, MapDirection.NORTH.previous());
+        assertEquals(MapDirection.SOUTH, MapDirection.WEST.previous());
+        assertEquals(MapDirection.EAST, MapDirection.SOUTH.previous());
     }
 }
